2a786bd040060fd444365d301bf1bd3bc3270c29
kevin alnizar <96331929+kevinxcode@users.noreply.github.com>
2024-04-27 11:09:52 +0700

    Create fe_login.js

---
+++ fe_login.js
@@ -0,0 +1,11 @@
+{
+    "i_err": 0,
+    "data": [
+        {
+            "emp_id": "123456",
+            "name": "KEVIN ALNIZAR",
+            "email": "kevin.alnizar@gmail.com",
+            "password": "demo!"
+        }
+    ]
+}
